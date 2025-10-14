75721a9cec98eb0393d7cf740fd2136de3490b3f
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-10-14 22:27:58 +0530

    updated etp parent page

--- src/app/products/effluentTreatmentPlant/page.tsx
+++ src/app/products/effluentTreatmentPlant/page.tsx
@@ -1,7 +1,20 @@
+import Footer from '@/Components/Footer/footer'
+import Header from '@/Components/Header/header'
 import React from 'react'
+import AppointmentBanner from '@/Components/Partials/appointmentBanner'
+import ProductHeroBg from '@/Components/Partials/productherobg'
+import EffluentTreatmentPlant from '@/Components/Products/EffluentTreatmentPlant'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <Header isWhite={true} />
+      <main className="max-w-screen-2xl">
+        <ProductHeroBg heading='Effluent Treatment <br/> Plant (ETP)' imageurl='/assests/ProductHeroBg.png'/>
+        <EffluentTreatmentPlant/>
+        <AppointmentBanner/>
+      </main>
+      <Footer />
+    </div>
   )
 }
