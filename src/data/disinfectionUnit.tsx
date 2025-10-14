5c8b4660092ae6ac28bdc58f1a7fc1467a84e1bb
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-10-14 23:53:01 +0530

    added DU static data

---
+++ src/data/disinfectionUnit.tsx
@@ -0,0 +1,140 @@
+import Chlorination from "@/assests/Products/DU/chlorination.png";
+import Ozonator from "@/assests/Products/DU/ozonator.png";
+import ECA from "@/assests/Products/DU/eca.png";
+import ASP from "@/assests/Products/DU/chlorination.png";
+export const disinfectionUnit = [
+  {
+    id: "chlorination",
+    label: "Chlorination",
+    title: "Chlorination",
+    image: Chlorination,
+    description: `
+    <style>
+    .description-content ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+    }
+    </style>
+    <div class="description-content">
+    <p>
+        Chlorination is the chemical disinfection of treated wastewater by using chlorine. This method removes pathogens and prevents waterborne illnesses.The pathogenic organisms include viruses, bacteria and protozoans. Cholera, dysentery, typhoid etc., are waterborne illnesses. Chlorination is essential to disinfect the water or make the water supplies potable. It is the stage before the water runs off into oceans, rivers and streams.
+    </p>
+    <br/>
+    <h2><strong>How Chlorine Destroys Microorganisms?</strong></h2>
+    <p>
+    Chlorine compounds destroy microbes by oxidising their cellular material. After oxidation, the cell membrane weakens, and chlorine enters the cell. As a result, it disrupts cell respiration and DNA activity. And, we know how these two processes are necessary for cell survival. So, by disrupting cell functionality, chlorine destroys different microbes.
+    </p>
+    <br/>
+    <h2><strong>Chlorine dosing</strong></h2>
+    <p>
+    When chlorine is dosed into a facility's water distribution system routinely it is injected continually in order to maintain a constant free chlorine residual.
+    Chlorine is usually dosed as a concentrated solution of sodium hypochlorite which, when dissolved in water, forms hypochlorous acid. Hypochlorous acid is the active disinfectant form of chlorine often described as 'free' or 'active' chlorine.
+    The combined concentration of hypochlorous acid and hypochlorites is often described as 'total chlorine'.
+    </p>
+    </div>
+  `,
+  },
+  {
+    id: "ozonation",
+    label: "Ozonation",
+    title: "Ozonator",
+    image: Ozonator,
+    description: `
+    <style>
+    .description-content ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+    }
+    </style>
+    <div class="description-content">
+    <p>
+    The chemical reaction that results in ozone is pretty simple. Ozone is a form of oxygen that is created when electrical energy breaks apart an ordinary oxygen molecule (O2) starting a chemical reaction that results in ozone (O3). Electrical energy breaks the ordinary O2 molecule into two O1 atoms The free oxygen at omsunite with other O2 molecules to produce zone (O1) + (O2) =(O3). Ozone is an unstable molecule because the 3rd oxygen atom is connected to the other two atoms with a weak bond. The weak bond is why ozone is such a powerful sanitizer.
+    </p>
+    <br/>
+    <h2><strong>Applications :-</strong></h2>
+    <ul>
+        <li>Powerful disinfectant to remove Salmonella, Listeria, E. coli, or any number of other dangerous microorganisms.</li>
+        <li>Colourless Treated Water.</li>
+        <li>Odourless Treated Water.</li>
+        <li>Suitable even if there is uneven supply of sewage.</li>
+        <li>Highly suitable for inconsistent sewage.</li>
+        <li>Overall organic &amp; inorganic content reduction (BOD &amp; COD), metals (iron, copper, and manganese), and inorganic forms of chloramine &amp; nutrients.</li>
+        <li>Less sludge production.</li>
+        <li>Low space requirement.</li>
+        <li>Does not require continuous operation.</li>
+        <li>Ozone is highly effective in cold water.</li>
+        <li>MLSS and SVI monitoring not required.</li>
+        <li>Environmentally safe to treat sewage water disposed in land or water.</li>
+        <li>Treated water suitable for flushing, irrigation, construction, and many other potable uses.</li>
+        <li>Environmentally preferred sanitizer, leaving behind only oxygen.</li>
+    </ul>   
+    <br/>
+    </div>
+  `,
+  },
+  {
+    id: "UVsterilize",
+    label: "UV Sterilize",
+    title: "Ultra-Violet Unit",
+    image: ECA, 
+    description: `
+    <style>
+    .description-content ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+    }
+    </style>
+    <div class="description-content">
+    <p>
+    An ultra violet unit coupled with asedimentremovalpre-filterisan effectiveway of controlling Bacterial contamination in a water supply where the water is not stored be fore use. The ultra violet unitissized according to the flow rate of the system.
+    </p>
+    <br/>
+    <h2><strong>Applications include:</strong></h2>
+    <p>
+    Well water treatment, manufacturing plants, agriculture, private wells, camp grounds, hotels, bottlers, aqua- culture, hospitals, food, restaurants, breweries, laboratories, marine, pharmaceutical, dairies and many other commercial applications.
+    </p>
+     <br/>
+    <h2><strong>How Ultraviolet Disinfection System Works:-</strong></h2>
+    <p>
+    Ultraviolet light alters the DNA material in cells so that bacteria, viruses, moulds, algae and other microorganisms can no longer reproduce. The microorganisms are considered dead, and the risk of disease from them is eliminated. These Ultraviolet Disinfection Systems are designed for commercial & industrial applications. UV Disinfection has an effective kill rate of 99.99% of most living microorganisms such as bacteria & viruses.
+    </p>
+    <br/>
+    <p>
+    <strong>Note: </strong> If you well water hashighiron,manganese, sulphurodour and orhard water you will need to provide proper pre-treatment before the water enters the UV system.
+    </p>
+       
+    </div>
+  `,
+  },
+  {
+    id: "ECA",
+    label: "ECA",
+    title: "ECA",
+    image: ECA,
+    description: `
+    <style>
+    .description-content ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+    }
+    </style>
+    <div class="description-content">
+    <p>
+      Electrolysed water (electrolyzed water, EOW, ECA, electrolyzed oxidizing water, electro-activated water or electro-chemically activated water solution) is produced by the electrolysis of ordinary tap water containing dissolved sodium chloride. The electrolysis of such salt solutions produces a solution of hypochlorous acid and sodium hydroxide. The resulting water can be used as a disinfectant.
+    </p>
+    <br/>
+    <h2><strong>Creation Process</strong></h2>
+    <p>
+      The electrolysis occurs in a specially designed reactor which allows the separation of the cathodic and anodic solutions. In this process, hydrogen gas and hydroxide ions can be produced at the cathode, leading to an alkaline solution that consists essentially of sodium hydroxide. At the anode, chloride ions can be oxidized to elemental chlorine, which is present in acidic solution and can be corrosive to metals. If the solution near the anode is acidic then it will contain elemental chlorine, if it is alkaline then it will comprise sodium hydroxide. The key to delivering a powerful sanitising agent is to form hypochlorous acid without elemental chlorine - this occurs at around neutral pH. Hypochlorous is a weak acid and an oxidizing agent. This "acidic electrolyzed water" can be raised in pH by mixing in the desired amount of hydroxide ion solution from the cathode compartment, yielding a solution of Hypochlorous acid (HOCl) and sodium hydroxide (NaOH). A solution whose pH is 7.3 will contain equal concentrations of hypochlorous acid and hypochlorite ion; reducing the pH will shift the balance toward the hypochlorous acid. At a pH between 5.5 and 6.0 approximately 90% of the ions are in the form of hypochlorous acid.[citation needed] In that pH range, the disinfectant capability of the solution is more effective than regular sodium hypochlorite (household bleach).
+    </p>
+    <br/>
+    <h2><strong>Efficient disinfectant</strong></h2>
+    <p>
+      Both sodium hydroxide and hypochlorous acid are efficient disinfecting agents; as mentioned above, the key to effective sanitation is to have a high proportion of hypochlorous acid present, this happens between acidic and neutral pH conditions.
+    EOW will kill spores and many viruses and bacteria.
+    Electrolysis units sold for industrial and institutional disinfectant use and for municipal water-treatment are known as chlorine generators. These avoid the need to ship and store chlorine, as well as the weight penalty of shipping prepared chlorine solutions. In March, 2016 inexpensive units have become available for home or small business users.
+    </p>
+    </div>
+  `,
+  },
+];
