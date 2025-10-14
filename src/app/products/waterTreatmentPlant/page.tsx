e5f93eaf79aaf9a1262f9e592876b3bbe8939f3b
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-10-14 23:06:33 +0530

    updated Water treatment plant parent component

--- src/app/products/waterTreatmentPlant/page.tsx
+++ src/app/products/waterTreatmentPlant/page.tsx
@@ -1,7 +1,21 @@
+import Footer from '@/Components/Footer/footer'
+import Header from '@/Components/Header/header'
 import React from 'react'
+import AppointmentBanner from '@/Components/Partials/appointmentBanner'
+import ProductHeroBg from '@/Components/Partials/productherobg'
+import WaterTreatmentPlant from '@/Components/Products/WaterTreatmentPlant'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <Header isWhite={true} />
+      <main className="max-w-screen-2xl">
+        <ProductHeroBg heading='Water Treatment <br/> Plant' imageurl='/assests/ProductHeroBg.png'/>
+        <WaterTreatmentPlant/>
+        <AppointmentBanner/>
+      </main>
+      <Footer />
+    </div>
   )
 }
+
