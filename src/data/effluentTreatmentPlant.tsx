083007cc87d0ae61cfa8923afa43b723c3de8ed5
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-10-14 22:29:33 +0530

    added ETP static data

---
+++ src/data/effluentTreatmentPlant.tsx
@@ -0,0 +1,51 @@
+import ETP from "@/assests/Products/ETP/etp.png";
+
+export const effluentTreatmentPlant = [
+    {
+        id: "mbbr",
+        label: "ETP",
+        title: "Effluent Treatment Plant (ETP)",
+        image: ETP,
+        description: `
+    <style>
+    .description-content ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+    }
+    </style>
+    <div class="description-content">
+    <p>
+        An Effluent Treatment Plant (ETP) is a specialized facility designed to treat industrial wastewater, Hospital OT and LAB waste water before its safe discharge into the environment. ETPs employ various physical, chemical, and biological processes to remove contaminants, ensuring compliance with environmental regulations and minimizing negative impacts on ecosystems and public health.
+    </p>
+    <br/>
+     <h2><strong>Process of Effluent Treatment Plant:</strong></h2>
+    <p>
+        ETP takes untreated wastewater as input(influent) and produces effluent as the output of this untreated wastewater and also extracts valuable substances from effluent. It runs through various steps for its successful working such as Equalisation, pH Control, Coagulation, Sedimentation, Filtration, Disinfection, & Sludge Drying.
+    </p>
+    <br/>
+    <h2><strong>Applicable areas of ETP:</strong></h2>
+    <ul>
+        <li>Food and processing industries</li>
+        <li>Pharmaceutical industry</li>
+        <li>Tannery &amp; Paper industries</li>
+        <li>Textile and dye industry</li>
+        <li>Dairy and Beverage industries</li>
+        <li>Paint Industry</li>
+        <li>Chemical Industries, etc.</li>
+    </ul>
+    <br/>
+    <h2><strong>Advantages of Effluent Treatment Plant:</strong></h2>
+    <ul>
+        <li>ETP converts wastewater into clean &amp; safe water.</li>
+        <li>It saves our money.</li>
+        <li>It protects our environment from pollution.</li>
+        <li>It protects humans from various diseases caused by waste waters.</li>
+        <li>ETP helps in minimizing waste.</li>
+        <li>It saves water by recycling the old or polluted water.</li>
+        <li>It helps in safely disposing of wastewater into the environment.</li>
+        <li>ETP eliminates the need to transport clean water.</li>
+    </ul>
+    </div>
+  `,
+    },
+];
