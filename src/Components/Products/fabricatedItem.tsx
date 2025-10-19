d5de68b1114874f5e8d2a844abc96c2dabd769a8
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-10-19 22:53:26 +0530

    added FI component and its static data

---
+++ src/Components/Products/fabricatedItem.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import React, { useRef, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { motion } from "framer-motion";
+import { textAnimationProps } from "@/animation/Framer";
+import Image, { StaticImageData } from "next/image";
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
+import { fabricatedItem } from "@/data/fabricatedItems";
+
+interface TabData {
+  id: string;
+  label: string;
+  title: string;
+  image?: StaticImageData;
+  description?: string;
+}
+
+const FabricatedItem: React.FC = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const tabSectionRef = useRef<HTMLDivElement | null>(null);
+
+  const handleNext = () => {
+    if (tabIndex < fabricatedItem.length - 1) {
+      setTabIndex(tabIndex + 1);
+      scrollToTabs();
+    }
+  };
+
+  const handlePrev = () => {
+    if (tabIndex > 0) {
+      setTabIndex(tabIndex - 1);
+      scrollToTabs();
+    }
+  };
+
+  const scrollToTabs = () => {
+    tabSectionRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+
+  return (
+    <section
+      ref={tabSectionRef}
+      className="w-screen min-h-screen pb-16 md:pb-24 px-4 md:px-12 xl:px-24 bg-white overflow-hidden"
+    >
+      <div className="container mx-auto">
+        <Tabs selectedIndex={tabIndex} onSelect={setTabIndex}>
+          <TabList className="flex border-b border-gray-300 mb-6 overflow-x-auto no-scrollbar mt-2 lg:mt-6 w-full">
+            {fabricatedItem.map((tab: TabData) => (
+              <Tab
+                key={tab.id}
+                className="flex-1 text-center px-4 py-3 text-base md:text-lg xl:text-xl text-[#233852] cursor-pointer hover:text-[#0195B1] focus:outline-none whitespace-nowrap transition-colors duration-300 font-montserrat font-semibold"
+                selectedClassName="border-b-4 border-[#0195B1] !text-[#0195B1] font-semibold"
+              >
+                {tab.label}
+              </Tab>
+            ))}
+          </TabList>
+
+          {fabricatedItem.map((tab: TabData, index: number) => (
+            <TabPanel key={tab.id}>
+              <div className="space-y-8 mt-8">
+                {tab.image && (
+                  <div className="flex justify-center">
+                    <Image
+                      src={tab.image}
+                      alt={tab.label}
+                      className="w-full md:max-w-4xl rounded-2xl shadow-lg"
+                      loading="lazy"
+                    />
+                  </div>
+                )}
+
+                <motion.h2
+                  className="text-3xl md:text-5xl xl:text-6xl text-[#0195B1] font-semibold font-montserrat mt-12"
+                  {...textAnimationProps}
+                >
+                  {tab.title}
+                </motion.h2>
+
+                {tab.description && (
+                  <div
+                    className="text-base md:text-lg leading-relaxed text-[#233852] font-epilogue font-normal text-justify space-y-4"
+                    dangerouslySetInnerHTML={{ __html: tab.description }}
+                  />
+                )}
+              </div>
+              <div className="flex flex-col md:flex-row justify-between items-center gap-4 mt-12 w-full">
+                {index > 0 ? (
+                  <button
+                    onClick={handlePrev}
+                    className="flex justify-center items-center gap-1.5 bg-[#233852] text-white px-4 py-2 sm:px-6 sm:py-3 rounded-lg hover:bg-[#0195B1] transition-all duration-300 font-montserrat cursor-pointer text-sm sm:text-base w-full md:w-auto text-center whitespace-normal break-words"
+                  >
+                    <FaArrowLeftLong /> Previous:{" "}
+                    {fabricatedItem[index - 1].label}
+                  </button>
+                ) : (
+                  <div className="w-full md:w-auto" />
+                )}
+
+                {index < fabricatedItem.length - 1 && (
+                  <button
+                    onClick={handleNext}
+                    className="flex justify-center items-center gap-1.5 bg-[#0195B1] text-white px-4 py-2 sm:px-6 sm:py-3 rounded-lg hover:bg-[#233852] transition-all duration-300 font-montserrat cursor-pointer text-sm sm:text-base w-full md:w-auto text-center whitespace-normal break-words"
+                  >
+                    Next: {fabricatedItem[index + 1].label}{" "}
+                    <FaArrowRightLong />
+                  </button>
+                )}
+              </div>
+            </TabPanel>
+          ))}
+        </Tabs>
+      </div>
+    </section>
+  );
+};
+
+export default FabricatedItem;
---
+++ src/data/fabricatedItems.tsx
@@ -0,0 +1,122 @@
+import decanter from "@/assests/Products/FI/Decanter.png";
+
+import screens from "@/assests/Products/FI/screens.png";
+
+export const fabricatedItem = [
+  {
+    id: "Decanter",
+    label: "Decanter",
+    title: "Decanter",
+    image: decanter,
+    description: `
+    <style>
+    .description-content ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+    }
+    </style>
+    <div class="description-content">
+    <p>
+        The decanter is a key piece of equipment within a sequencing batch reactor (SBR) system, removing clarified effluent from the basin during the decant phase of the operational cycle. The sbr process is operated using defined cycle times adjusted to variable flow conditions. Each cycle typically consists of three main phases; aerate, settle and decant. The basin is aerated continuously during the first half of the cycle. After the aeration phase, the system goes into a settling phase where liquid/solids separation occurs. In the final step, clear water is decanted out of the basin.The decanter typically is installed on the basin wall supported by an effluent box on one end and by a pedestal on the other. The decanter is lowered and raised using an electro-mechanical actuator. The actuator moves between top and bottom limit switches whenever the decanter is in operation, ensuring that the decanter travels from the "park" position to the bottom water level (BWL). The decanter parks above top water level during the aeration and settling phases of the cycle, thereby eliminating any possibility of solids carryover during these periods.
+    </p>
+    <br/>
+    <h2><strong>Operation</strong></h2>
+    <p>
+        During the decant phase, after receiving a signal from the control system, the decanter travels from the "park" position to the design BWL. Upon reaching the BWL, the decanter pauses for a few minutes to ensure completion of the discharge flow over the weir. The decanter then returns to the "park" position, where it remains until the start of the next decant phase. Redundant limit switches on the actuator ensure reliable operation whenever the decanter is in motion. The limit switches fully integrate with process control time overrides and interlocks, thereby eliminating the potential for blower activation during decant. At "park" position, the decanter provides "fail safe" overflow protection in the event of a power failure. Settled supernatant will flow over the decanter weir and into the effluent collection system. The decanter scum guard float prevents carry over of any floatables during such emergency periods. The decanter weir always is visible from the basin walkway. This feature provides the operator with a continuous visual check of the effluent quality.
+    </p>
+    </div>
+  `,
+  },
+  {
+    id: "FilterVessels",
+    label: "Filter Vessels",
+    title: "Filter Vessels",
+    // image: SBR,
+    description: `
+    <style>
+    .description-content ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+    }
+    </style>
+    <div class="description-content">
+    <p>
+    Vessel filter is also called media filtration system, it is a kind of filter media to remove suspended solids and other fine particles in water, and finally reduce the turbidity of water and filter the water with high efficiency. Purifying quality effect. , Commonly used filter materials are Pebbles, coarse sands, different type sands, activated carbon, anthracite, manganese dioxide (MNo2) and Katalox lite etc. Vessel Filter is widely used in R O system, home purpose, garden irrigation, petroleum, chemical, metallurgy and other industries.
+    </p>
+    </div>
+  `,
+  },
+  {
+    id: "screens",
+    label: "Screens",
+    title: "Screens",
+    image: screens,
+    description: `
+    <style>
+    .description-content ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+    }
+    </style>
+    <div class="description-content">
+    <h2><strong>Basket & Rotary Screen</strong></h2>
+    <h2><strong>BASKET SCREEN :-</strong></h2>
+    <p>
+    The screen basket is constructed of stainless steel sections. When the screen basket is raised,the feed is automatically blocked by a drop screen so that no screenings can get into the outlet channel while the basket is being emptied. Suitable for deep, small space requirement. The screen allows the passage of grit without mal functioning. Screenings conveyed positively to the discharge point. Excellent cost/performance ratio and easy maintenance.
+    </p>
+    <br/>
+    <h2><strong>WIRE SPACING:</strong>1 mm to 6 mm </h2>
+    <br/>
+    <h2><strong>MATERIAL OF CONSTRUCTION:</strong> Stainless steel 304, 316 & other grades on request</h2>
+    <br/>
+    <h2><strong>AUTOMATIC ROTARY SCREEN :-</strong></h2>
+    <br/>
+    <p>
+    <strong>APPLICATION: </strong>Screen is a mechanically cleaned fine screening equipment used to prevent fine sized floating wastes from travelling further in to waste water treatment plants. screen is designed to screen out almost all the fine floating wastes such as plastic bags, pouches, sachets, paper wastes cloths, condoms, weeds, and various other fine fibrous wastes coming with waste water or effluents.
+    </p>
+    <br/>
+    <h2><strong>WIRE SPACING: 1 mm to 6 mm </strong> </h2>
+    <h2><strong>MATERIAL OF CONSTRUCTION: Stainless steel 304, 316 & other grades on request</strong> </h2>  
+    </div>
+  `,
+  },
+   {
+    id: "VolutePress",
+    label: "Volute Press",
+    title: "Volute Press",
+    // image: MBR,
+    description: `
+    <style>
+    .description-content ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+    }
+    </style>
+    <div class="description-content">
+    <p>
+    It reaches the goal of extrusion dewatering sludge by the powerful extrusion pressure of changing the screw diameter and distance, and the tiny spacing between movable annular plates and fixed annular plates.
+    </p>
+    <br/>
+    <h2><strong>Working Process</strong></h2>
+    <br/>
+    <p>
+    Flow control tank: Sludge feed is regulated with the overflow pipe, returning excess volume to the sludge storage tank. Flocculation tank: Polymer flocculant and sludge are stirred and mixed, forming floc suitable for the Volute Dewatering Press. Then, the built-in thickener in the tank instantly thicken the sludge. Cylinder unit: Sludge is further thickened in the thickening zone of the cylinder, and then the inner pressure increased at the dewatering zone helps sludge being dewatered well. Discharge outlet for dewatered cake: Further pressure is applied from the outlet side with the end plate, discharging dewatered case with 20+-5% solid content
+    </p>
+    </br/>
+    <h2><strong>Application</strong></h2>
+    <br/>
+    <ul>
+    <li>Municipal water and wastewater treatment plants</li>
+    <li>Agricultural/Fishery community wastewater treatment plants</li>
+    <li>Industrial waste treatment plants</li>
+    <li>Food/Beverage production plants</li>
+    <li>Dairy farming/Meat processing plants</li>
+    <li>Chemicals manufacturing plants</li>
+    <li>Machinery manufacturing/Metal processing plants</li>
+    <li>Laundry wastewater</li>
+    <li>Other (paper manufacturing, building material manufacturing, oilfield associated water accompanied with crude oil mining, etc.)</li>
+    </ul>
+    </div>
+  `,
+  },
+];
